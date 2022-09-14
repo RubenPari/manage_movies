d30e9f606d624651003ca55b41df9eb421aa28ea
RubenPari <rubenpari.developer@gmail.com>
2022-09-14 11:24:44 +0200

    mapped add movie endpoint

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 require 'net/http'
 
 class MoviesController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def exists
     if !params[:q] || params[:q] == ''
       render json: { "status" => "error", "message" => "No query provided" }, status: 400
@@ -21,6 +23,37 @@ class MoviesController < ApplicationController
         response_body = response_body["results"][0]
 
         render json: { "status" => "ok", "founded" => "ok", "result" => response_body }, status: 200
+      end
+    end
+  end
+
+  def create
+    if !params[:id] || params[:id] == ''
+      render json: { "status" => "error", "message" => "No id provided" }, status: 400
+    else
+      endpoint = ENV['BASE_URL'] + "/Title/" + ENV['API_KEY'] + "/" + params[:id]
+
+      response = Net::HTTP.get_response(URI(endpoint))
+
+      if response.code != "200"
+        render json: { "status" => "error", "message" => response.body }, status: response.code
+      else
+        # convert the response to a object
+        response_body = JSON.parse(response.body)
+
+        # add movie to db
+        movie = Movie.new do |m|
+          m.id = response_body["id"]
+          m.title = response_body["title"]
+          m.description = response_body["plot"]
+          m.image_url = response_body["image"]
+        end
+
+        if movie.save
+          render json: { "status" => "ok", "result" => "movie added successfully" }, status: 200
+        else
+          render json: { "status" => "error", "message" => movie.errors }, status: 500
+        end
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'movies/exists', to: 'movies#exists'
+  post 'movies/add', to: 'movies#create'
 end
